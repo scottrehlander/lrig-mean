c50f2e626f0361010bd2e2dc083ff4f12ec75495
Scott Rehlander <scott@stackwave.com>
2016-10-20 10:58:42 -0400

    - Remove Articles from the main nav

--- modules/chapters/client/chapters.client.module.js
+++ modules/chapters/client/chapters.client.module.js
@@ -4,4 +4,5 @@
   app.registerModule('chapters', ['core']);// The core module is required for special route handling; see /core/client/config/core.client.routes
   app.registerModule('chapters.routes', ['ui.router', 'core.routes', 'articles.services']);
   app.registerModule('chapters.services');
+
 }(ApplicationConfiguration));
